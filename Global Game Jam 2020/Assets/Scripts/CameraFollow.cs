72791cfee2ae74296c3bb9e2f0e63ef2343797d7
Mason Eastman <masoneastman555@gmail.com>
2020-01-31 23:28:16 -0600

    Camera Follow

---
+++ Global Game Jam 2020/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //allows for the correct gameobject that the camera should follow to be set from the inspector
+    public Transform trackedObject;
+
+    //Camera's position is tied to the center of the player's position
+    //updates every frame
+    void Update()
+    {
+        transform.position = new Vector3(trackedObject.position.x, trackedObject.position.y, transform.position.z);
+    }
+}
