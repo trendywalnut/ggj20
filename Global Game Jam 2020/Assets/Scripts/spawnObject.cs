54bdc299f35a08657b32db1c908c6d8f45587c20
jehorak <jehorak@sbcglobal.net>
2020-02-01 18:09:27 -0600

    procgen levels and shid

---
+++ Global Game Jam 2020/Assets/Scripts/spawnObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour
+{
+
+    public GameObject[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        GameObject instance = (GameObject)Instantiate(objects[rand], transform.position, Quaternion.identity);
+        instance.transform.parent = transform;
+    }
+}
