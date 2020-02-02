933565b2080d270adffca7bbd8feb959ce80b15b
Mason Eastman <masoneastman555@gmail.com>
2020-02-02 14:35:02 -0600

    GOOD SHIT

---
+++ Global Game Jam 2020/Assets/TransitionChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Transition");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
